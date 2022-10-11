dc81c59811c7e35c45ce522c42888d372336e1c5
t-kalden <kalden.t@outlook.com>
2022-10-11 17:46:06 -0400

    Created Alert reducer and context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -10,22 +11,24 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-          
-          <main className='container mx-auto px-2 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home/>} />
-              <Route path='/about' element={<About/>} />
-              <Route path='/*' element={<NotFound/>} />
-              <Route path='/notfound' element={<NotFound/>} />
-            </Routes>
-          </main>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            
+            <main className='container mx-auto px-2 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/*' element={<NotFound/>} />
+                <Route path='/notfound' element={<NotFound/>} />
+              </Routes>
+            </main>
 
-          <Footer/>
-        </div>
-      </Router>
+            <Footer/>
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
